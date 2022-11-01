29989a601ec03fda48fb9454df92b26edecc7454
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-01 12:31:22 +0200

    Update Program.cs

--- Homework_1_Filatov_Vladyslav_Serhiyovych/Task1/Program.cs
+++ Homework_1_Filatov_Vladyslav_Serhiyovych/Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Homework_1_Filatov_Vladyslav_Serhiyovych;
+// Вітаю. Все доступно.
 
 Product product = new Product("М'ясо", 150, 2);
 Console.WriteLine(product);
@@ -7,7 +8,8 @@ Buy buy = new Buy(product, 3);
 Console.WriteLine(buy);
 
 Console.WriteLine();
-
+// Не логічно передавати і продукт і покупку, яка містить інформацію про продукт. 
+// Крім того покупка можу містити кілька продуктів.
 Check chek = new Check(product, buy);
 
 
