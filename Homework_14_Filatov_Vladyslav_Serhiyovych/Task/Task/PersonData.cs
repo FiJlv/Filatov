6497ce57a5465610f2c8abe50e4f017fa27620b8
Vladyslav Filatov <vladzzzzxd@gmail.com>
2023-01-19 15:03:58 +0200

    HW 14

---
+++ Homework_14_Filatov_Vladyslav_Serhiyovych/Task/Task/PersonData.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class PersonData
+    {
+        SqlConnection sqlConnection;
+        public PersonData(string connectionString)
+        {
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public void Insert(string id, string firstName, string lastName)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Person (PersonId, FirstName, LastName)" +
+                $" VALUES ('{id}', '{firstName}', '{lastName}')", sqlConnection);
+                sqlConnection.Open(); 
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong" + e);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        public void Update(string id, string firstName, string lastName)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand($"UPDATE Person " +
+                    $"SET FirstName = '{firstName}', LastName = '{lastName}' " +
+                    $"WHERE PersonId = '{id}'", sqlConnection);
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong" + e);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        public void Read()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Person", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Console.WriteLine($"{dataReader["FirstName"]} {dataReader["LastName"]}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong" + e);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public void Delete(string personId)
+        {           
+            try
+            {
+                SqlCommand cm = new SqlCommand($"delete from person where personid = '{personId}'", sqlConnection);
+                sqlConnection.Open();
+                cm.ExecuteNonQuery();
+                Console.WriteLine($"{personId} deleted successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong" + e);
+            }
+            
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ Homework_14_Filatov_Vladyslav_Serhiyovych/Task/Task/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Data.SqlClient;
+using Task;
+
+string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+PersonData personData = new PersonData(connectionString);
+
+personData.Insert("1", "Thomas", "Johnson");
+personData.Insert("2", "Jhon", "Smith");
+personData.Read();
+
+Console.WriteLine();
+
+personData.Update("2", "John", "Smith");
+personData.Read();
+
+Console.WriteLine();
+
+personData.Delete("1");
+personData.Delete("2");
